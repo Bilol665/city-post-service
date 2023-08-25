01cbc92a54db6d7a36b911e6dab7570dc76041a5
Bilol665 <bilolsultan665@gmail.com>
2023-08-25 07:50:31 +0500

    Search and lenta!

--- src/main/java/uz/pdp/citypostservice/repository/post/PostRepository.java
+++ src/main/java/uz/pdp/citypostservice/repository/post/PostRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.citypostservice.domain.entity.post.PostEntity;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, UUID> {
     Optional<PostEntity> findPostEntityByOwnerId(UUID ownerId);
+
+    List<PostEntity> findPostEntitiesByNameContainingIgnoreCase(String name);
 }
